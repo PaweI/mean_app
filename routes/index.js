9fb9ded75462b355aac40118242d47e5a7f60d8b
Pavel Redics <pavel.redics@gmail.com>
2014-12-30 22:17:35 +0000

    add todos info into indexjs and index.jade

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Express',
+    todos: [
+      { description : "Buy eggs",
+        due : new Date(new Date().getTime() + 24 * 60 * 60 * 1000),
+        done : false
+      },
+      { description : "Write next blog post",
+        due : new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000),
+        done : false
+      },
+      { description : "Build todo list app",
+        due : new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000),
+        done : false
+      },
+    ] 
+  });
 });
 
 module.exports = router;
