7429972990c3c6f6acd4b77a87dd21c8059e41fe
Pavel Redics <pavel.redics@gmail.com>
2014-12-31 23:33:20 +0000

    added more tests

--- tests/e2e/spec.js
+++ tests/e2e/spec.js
@@ -1,7 +1,30 @@
-describe('angularjs homepage', function() {
-  it('should have a title', function() {
+// describe('angularjs homepage', function() {
+//   it('should have a title', function() {
+//     browser.get('http://localhost:3000/');
+
+//     expect(browser.getTitle()).toEqual('Express');
+//   });
+// });
+
+describe('Todo list', function() {
+  beforeEach(function() {
     browser.get('http://localhost:3000/');
 
-    expect(browser.getTitle()).toEqual('Express');
+    sleep(1);
+  });
+
+  it("should move todo to correct list when user toggles checkbox", function() {
+    expect(repeater('.not-done').count()).toBe(2);
+    expect(repeater('.done').count()).toBe(1);
+
+    element('.not-done:nth-child(1) input').click();
+    sleep(0.1);
+    expect(repeater('.not-done').count()).toBe(1);
+    expect(repeater('.done').count()).toBe(2);
+
+    element('.done:nth-child(1) input').click();
+    sleep(0.1);
+    expect(repeater('.not-done').count()).toBe(2);
+    expect(repeater('.done').count()).toBe(1);
   });
 });
