53c556b5fb95d293c28cd347fc6fc1e94afc82e6
Pavel Redics <pavel.redics@gmail.com>
2014-12-31 13:34:56 +0000

    list tests in terminal

--- tests/e2e/conf.js
+++ tests/e2e/conf.js
@@ -1,5 +1,12 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['spec.js']
+  specs: ['spec.js'],
+
+// Options to be passed to Jasmine-node.
+  jasmineNodeOpts: {
+      showColors: true, // Use colors in the command line report.
+      isVerbose: true, // List all tests in the console
+      includeStackTrace: true
+  }
 }
 
